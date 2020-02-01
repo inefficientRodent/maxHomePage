c967ca6ac7b234a28610546d9cb9503114ff7cfb
inefficientRodent <60514699+inefficientRodent@users.noreply.github.com>
2020-02-01 20:55:08 +0000

    Overall updates to layout
    Finally did the tabs and stuff properly, christ that was way too long.

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ var gameData = {
 	food: 0,
 	day: 0,
 	season: 'spring',
+	temperature: 'cold',
 	health: 10,
 }
 
 /*Things that make the game have content*/
-
-
-
+var gameLoop = window.setInterval(function() {
+	
+}, 1000)
 
 
 
 /*The Stupid Stuff That Makes Things Work Or Whatever*/
+var myVar;
+function loadFunction() {
+	myVar = setTimeout(showPage, 1000)
+}
+function showPage() {
+	document.getElementById("loadScreen").style.display = "none";
+	document.getElementById("titleWrap").style.display = "block";
+	document.getElementById("fuckSake").style.display = "block";
+	document.getElementById("navigation").style.display = "flex";
+	document.getElementById("playerBase").style.display = "block";
+	document.getElementById("gameContainer").style.display = "block";
+}
+
 function tab(tab) {
 	document.getElementById("playerBase").style.display = "none"
 	document.getElementById("Forest").style.display = "none"
@@ -26,4 +40,5 @@ function tab(tab) {
 
 var mainLoop = window.setInterval(function() {
 	document.getElementById("pBaseMessage").innerHTML = "You are at your base. You think it's probably " + gameData.season + "."
+	document.getElementById("pBaseTemp").innerHTML = "It feels " + gameData.temperature + "."
 }, 1000)
